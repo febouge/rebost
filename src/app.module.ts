45074c57be52b4b25d695dba3b7984cc2ac075d7
Ferran <febouge@protonmail.ch>
2018-12-04 09:59:32 +0100

    Update gitlab CI to run e2e tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,26 @@ import { AllergenModule } from './allergen/allergen.module';
 import { UnitModule } from './unit/unit.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
+require('dotenv').config();
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_SCHEMA,
+      entities: ['src/**/**.entity{.ts,.js}'],
+      synchronize: false,
+      logging: true,
+      migrationsTableName: 'custom_migration_table',
+      migrations: ['./migration/*.ts'],
+      cli: {
+        migrationsDir: 'migration'
+      }
+    }),
     RecipeModule,
     IngredientModule,
     TagModule,
